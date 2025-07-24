8d73ac8ce1cb96ff3289c65cada4679132574753
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2025-07-23 22:18:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numberDisplay = document.querySelector('p')
+const display = document.querySelector('p')
 const keypad = document.querySelectorAll('.key')
 const mathOps = document.querySelectorAll('.op')
 const button = document.querySelector('button')
@@ -7,18 +7,23 @@ const enterButton = document.getElementById('enter')
 const operationElements = []
 
 let keyValue
+let operValue
 
 keypad.forEach(button => {
     button.addEventListener('click', function() {
         keyValue = this.value
+        display.innerText = keyValue
         console.log('clicked button value:', keyValue)
+        operationElements.push(keyValue)
+        console.log(operationElements)
     })
 })
 
 mathOps.forEach(button => {
     button.addEventListener('click', function() {
-        keyValue = this.value
-        console.log('clicked button op value:', keyValue)
+        operValue = this.value
+        display.innerText = operValue
+        console.log('clicked button op value:', operValue)
     })
 })
 
@@ -26,20 +31,33 @@ let result
 let data1
 let data2
 
+// function enter()    {
+//     button.addEventListener('click', function() {
+//     if (operValue=+) {
+//         add()
+//     } else if (operValue=-) {
+//         sub()
+//     } else if (operValue = *) {
+//         mult()
+//     } else {
+//         div()
+//     }
+//     })
+// }
 
 function add()  {
-    result = data1 + data2 
+    result = operationElements[0] + operationElements[1] 
 }
 function sub()  {
-    result = data1 - data2
+    result = operationElements[0] - operationElements[1]
 }
 function mult() {
-    result = data1 * data2
+    result = operationElements[0] * operationElements[1]
 }
 function div()  {
-    if (data2=0) {
+    if (operationElements[1]=0) {
         alert("Error: impossible operation")
-    } else {result = data1 / data2}
+    } else {result = operationElements[0] / operationElements[1]}
 }
 let selectedNumOp
 
