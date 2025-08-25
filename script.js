694800b99e46a145e63e17828d34e056dd07a936
Didier Emmanuel TAVARES <didier.tavares@gmail.com>
2025-08-25 17:03:30 -0300

    work in progress > clear e enter funcionando

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const display = document.querySelector('p')
+let display = document.querySelector('p')
 const keypad = document.querySelectorAll('.key')
 const mathOps = document.querySelectorAll('.op')
 const button = document.querySelector('button')
 const enterButton = document.getElementById('enter')
+const clearDisplay = document.getElementById('clear')
+
 
 const operationElements = []
 
 let keyValue
 let operValue
+display.innerText = Number('0')
 
 keypad.forEach(button => {
     button.addEventListener('click', function() {
@@ -32,50 +35,34 @@ let data1
 let data2
 
 function calculus() {
-    result = Number(operationElements[0]).Number(operValue).Number(operationElements[1])
+    data1 = Number(operationElements[0])
+    data2 = Number(operationElements[1])
+    if (operValue === '+') {
+        result = data1 + data2
+    } else if (operValue === "-") {
+        result = data1- data2
+    } else if (operValue === "*") {
+        result = data1*data2
+    } else {
+        if (data2 = 0) {
+            result = "Error: operation impossible"
+        } else {
+            result = data1 / data2
+        }
+    }
     console.log(result)
+    display.innerText = result
+    operationElements.splice(1)
+    console.log(operationElements.length)
+    operationElements[0] = result
+    console.log(operationElements[0])
 }
 
-
-//     if (operValue = +) {
-//         add()
-//     } else if (operValue =-) {
-//         sub()
-//     } else if (operValue =*) {
-//         mult()
-//     // } else {
-//     //     div()
-//     }
-
-enter.addEventListener('click', calculus)
-
-function add()  {
-    result = operationElements[0] + operationElements[1] 
+function clearScreen() {
+    display.innerText = Number('0')
+    operationElements.length = 0
+    console.log(operationElements)
 }
-function sub()  {
-    result = operationElements[0] - operationElements[1]
-}
-function mult() {
-    result = operationElements[0] * operationElements[1]
-}
-function div()  {
-    if (operationElements[1]=0) {
-        alert("Error: impossible operation")
-    } else {result = operationElements[0] / operationElements[1]}
-}
-let selectedNumOp
 
-// function showClick()    {
-//     console.log('keypad was clicked')
-//     alert('clicked!')
-// }
-
-// function selection() {
-
-//         operationElements.push(selectedNumOp)
-//     console.log(selectedNumOp)
-// }
-
-// function display()  {
-//     numberDisplay.innerText = 
-// }
+enterButton.addEventListener('click', calculus)
+clearDisplay.addEventListener('click', clearScreen)
